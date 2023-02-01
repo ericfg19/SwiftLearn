e481f00e30d6949e2f14631574e84b72963501fc
ericfg19 <eric_fg@live.com.ar>
2023-02-01 11:41:08 -0300

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,10 @@
+var greetings = "Primeiro print em Swift"
+
+print(greetings)
+
+var myName = "Eric"
+let myAge = 31
+
+let message = myAge >= 18 ? "Você, "+myName+", é um adulto" : "Você, "+myName+", é menor de idade"
+
+print(message)
