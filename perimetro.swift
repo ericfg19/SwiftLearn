2fb8e4d87a0cae6630b94836a792166a77a16417
EricFG <eric_fg@live.com.ar>
2023-02-16 21:27:25 -0300

    add calc perimetro

---
+++ perimetro.swift
@@ -0,0 +1,8 @@
+//calcular o perímetro de um retângulo
+
+let comprimento = 5.0 
+let largura = 3.0 
+
+let perimetro = 2 * (comprimento + largura) 
+
+print("O perímetro do retângulo é: \(perimetro)")
